68d437eacb8ad6786b9918dfd80a7a2f271ff4bc
Mahatma Kollu <hatmatyt@gmail.com>
2021-09-16 19:18:28 -0400

    setting up scripts
    utilizes rojo & knit

---
+++ src/server/ServerRuntime.server.lua
@@ -0,0 +1,25 @@
+local Knit = require(game:GetService("ReplicatedStorage").Knit)
+local Promise = require(Knit.Util.Promise)
+local Component = require(Knit.Util.Component)
+
+function Knit.OnComponentsLoaded()
+    if Knit.ComponentsLoaded then
+        return Promise.resolve()
+    end
+    return Promise.new(function(resolve)
+        local heartbeat
+        heartbeat = game:GetService("RunService").Heartbeat:Connect(function()
+            if Knit.ComponentsLoaded then
+                heartbeat:Disconnect()
+                resolve()
+            end
+        end)
+    end)
+end 
+
+Knit.AddServices(script.Parent.Services)
+
+Knit.Start():Then(function()
+    Component.Auto(script.Parent.Components)
+    Knit.ComponentsLoaded = true
+end):Catch(warn)
