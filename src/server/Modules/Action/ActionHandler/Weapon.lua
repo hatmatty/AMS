7e262b04bdb79d1d0b5aaa95d08b1d71f66f0f0a
Infinitum <hatmatty@users.noreply.github.com>
2021-09-24 17:27:48 -0400

    tool update

--- src/server/Modules/Action/ActionHandler/Weapon.lua
+++ src/server/Modules/Action/ActionHandler/Weapon.lua
@@ -20,6 +20,25 @@ Weapon.InputInfo = {
 
 function StartDraw(Action)
     local tool = Action.PrimaryTool
+    for _,sibling in pairs(tools.Siblings) do
+        if sibling.Config.ActionHandler == "Shield":
+            if sibling.Actions["Block"] then
+                sibling.Actions["Block"]:End()
+            end
+        local signal = Signal.new()
+     
+        sibling:Lock(signal, 
+            {
+                [Enum.UserInputState.End] = {
+                    [Enum.UserInputType.MouseButton2] = {Name = "StopBlock"}
+                },
+            }, 
+            {
+                ["StopBlock"] = Action.new("StopBlock", function(Action)
+                    
+                end)
+            })
+    end
     
 end
 
@@ -32,6 +51,7 @@ Weapon:StoreAction(Action.new("Draw", StartDraw, EndDraw))
 
 function ReleaseStart(Action)
     local tool = Action.PrimaryTool
+    tool.Actions["Release"]:End()
     
     
     Action:End()
