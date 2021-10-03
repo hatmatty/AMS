0815be37acde345d6a7cc526df47fa36047f5cd0
Mahatma Kollu <hatmatyt@gmail.com>
2021-10-03 17:06:03 -0400

    animation export change

--- src/shared/Config.lua
+++ src/shared/Config.lua
@@ -1,34 +1,27 @@
 local Config = {}
 
+Config.WalkAnimation = 6492892050
+
 Config.StunnedAnimations = {
-	StunStab =7129179801,
+	StunStab = 7129179801,
 	StunLeft = 7129183472,
 	StunRight = 7129034287,
 }
 
 local DefaultWeaponAnims = {
-	-- Weapon
-	DrawRight = 7561804876,
-	DrawStab = 7566841005,
-
-	ReleaseRight = 7562183296, 
-	ReleaseStab = 7571425026,
-
 	Left = 7632564332,
 	Right = 7632959453,
 	Stab = 7632993927,
 
-	-- Essential 
-	--InitHolster = 7554818252,
 	Holster = 7566107081,
-	--InitEquip = 7554827098,
 	Equip = 7532918137,
 }
 
 local DefaultShieldAnims = {
+	Block = 7566699306,
+	
 	Holster = 7566057875,
 	Equip = 7566034212,
-	Block = 7566699306,
 }
 
 Config.Gore = true
