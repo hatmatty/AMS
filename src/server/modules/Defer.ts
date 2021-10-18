e7e5327c6ee243bb5352cff6636dcb1f1b62e99e
Infinitum <hatmatty@users.noreply.github.com>
2021-10-18 08:07:56 -0400

    Update Defer.ts

--- src/server/modules/Defer.ts
+++ src/server/modules/Defer.ts
@@ -1,3 +1,8 @@
+
+/**
+ * Defers a callback.
+ * @parm Callback - a function that is called after being deffered
+ */
 export function Defer(func: Callback) {
 	Promise.defer<void>((resolve, reject) => resolve()).then(() => func());
 }
