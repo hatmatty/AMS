5485073225e466732b10d2bc9f9d6f60b5af0e8d
Infinitum <hatmatty@users.noreply.github.com>
2021-10-18 11:16:23 -0400

    Update ToggleButton.ts

--- src/server/services/ToggleButton.ts
+++ src/server/services/ToggleButton.ts
@@ -5,13 +5,26 @@ import { CollectionService, Players } from "@rbxts/services";
 import { ITool } from "server/components/Tool";
 import { ToolAdded, ToolRemoved } from "./ToolService";
 
+/**
+ * Hooks into ToolService's ToolAdded and ToolRemoved events and updates all of a player's tool to have updated buttons and fires a button event with informoration on the new buttons.
+ */
 @Service({})
 export class ToggleButton implements OnInit {
+	/**
+	 * Connects to the ToolAdded and ToolRemoved and calls ConfigureToggleButtons with the arguments from the connections.
+	 */
 	onInit() {
 		ToolAdded.Connect((store, parent) => this.ConfigureToggleButtons(store, parent));
 		ToolRemoved.Connect((store, parent) => this.ConfigureToggleButtons(store, parent));
 	}
-
+	
+	/**
+	 * 
+	 * Takes in a copy of a state from the tool store in toolservice and a parent which is the name of a player and re-configures all of the BUTTON_TOGGLE attributes of the player's tools.
+	 * 
+	 * @param state - a copy of a state from toolservice
+	 * @param parent - a string that will cause an error if it does not match up to a player's username in the game
+	 */
 	private ConfigureToggleButtons(state: State, parent: string) {
 		const player = this.GetPlayerFromUserName(parent);
 
@@ -48,14 +61,24 @@ export class ToggleButton implements OnInit {
 			index++;
 		});
 	}
-
+	
+	/**
+	 * Gets a tool object and returns a number which indicates what priority in sorting tool buttons the tool should have (higher then number means the tool will be sorted closer to first)
+	*/
 	private getOrder(tool: ITool): number {
 		if (CollectionService.HasTag(tool.instance, "Weapon")) {
+			return 2;
+		} else if (CollectionService.HasTag(tool.instance, "Shield")) {
 			return 1;
+		} else {
+			return 0;
 		}
-		return 0;
 	}
 
+	/**
+	 * @param playerName - a player's username
+	 * @returns a Player if it found a player from the username provided or undefined.
+	 */
 	private GetPlayerFromUserName(playerName: string): Player | undefined {
 		// eslint-disable-next-line roblox-ts/no-array-pairs
 		for (const player of Players.GetChildren()) {
